3d8be99b148d5e2aed6ae960b2a9e6f53c1dd353
0xdivi-code <hallbryghtaa@gmail.com>
2026-03-14 16:58:34 +0100

    url

--- public/config.js
+++ public/config.js
@@ -28,7 +28,7 @@ window.__RUNTIME_CONFIG__ = {
   VITE_TWITTER_URL: "",
   VITE_SEO_SITE_NAME: "",
   VITE_SEO_SITE_DESCRIPTION: "",
-  VITE_SEO_SITE_URL: "#",
+  VITE_SEO_SITE_URL: "https://vantide-perps-uehy.vercel.app/",
   VITE_SEO_SITE_LANGUAGE: "en",
   VITE_SEO_SITE_LOCALE: "en_US",
   VITE_SEO_TWITTER_HANDLE: "",
